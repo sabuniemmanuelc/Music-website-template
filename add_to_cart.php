58d80313463524a28c014fd162b1e6d005236574
Sabuni Emmanuel C <sabuni.emmanuelc@gmail.com>
2020-10-23 00:02:10 +0200

    Add files via upload

---
+++ add_to_cart.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Sabuni Emmanuel">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Oryx Accapella</title>
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+
+    <link rel="stylesheet" href="css/studio4nx.css"/>
+
+    <link rel="icon" href="assets/images/std4n.png" type="image/x-icon" />
+
+</head>
+<body>
+    <section class="">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <img src="img/album_art/albumart.png" alt="">
+            </div>
+            <div class="col-12 col-lg-6 px-5 mt-lg-5">
+                <h2 class="mb-lg-2">DELUXE 2CD</h2>
+                <h4 class="py-2 font-weight-bold">125 ZMW</h4>
+                <div class="py-2">
+                    <p class="py-2 font-weight-bold">Bundle Includes</p>
+                    <ul>
+                        <li class="py-2">Full Album: with extra tracks</li>
+                        <li class="py-2">Coupon to the next concert</li>
+                        <li class="py-2">Updates and Discount on the next new album</li>
+                    </ul>
+                    <input type="number" class="rounded-0 py-3 px-2" style="width:100px" name="" id="" min="0" placeholder="0"> <input type="button" class="btn btn-dark rounded-0 py-3 w-50" style="font-size:19px" value="ADD TO CART">
+                </div>
+                <div class="py-2 font-weight-bold">
+                   <a  class="text-dark" data-toggle="collapse" href="#details" role="button" aria-expanded="false" aria-controls="details">Details  <hr></a>
+                </div>
+                <div class="collapse" id="details">eeeeeeeeeeeeeeeeeeeee</div>
+                <div class="py-2 font-weight-bold">
+                <a  class="text-dark" data-toggle="collapse" href="#track" role="button" aria-expanded="true" aria-controls="track"> Track List <hr></a>
+                </div>
+                <div class="collapse" id="track">
+                    <ul>
+                        <li>In the end</li>
+                        <li>One step closer to the edge</li>
+                        <li>numb</li>
+                        <li>Place for my head</li>
+                        <li>Sabuni Emmanuel</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- jQuery -->
+<script src="js/jquery-3.4.1.slim.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Sabuni Emmanuel">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Oryx Accapella</title>
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="fontawesome-free-5.12.0-web/css/all.min.css">
+
+    <link rel="stylesheet" href="css/studio4nx.css"/>
+
+    <link rel="icon" href="assets/images/std4n.png" type="image/x-icon" />
+
+</head>
+
+<body class="bg-main">
+    <header class="py-lg-4">
+        <nav class=" container navbar navbar-expand-lg nav-black sticky-top text-color">
+            <a class="navbar-brand text-color">Oryx Accapella</a>
+            <button class="navbar-toggler text-white-50" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="my-nav" class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto text-color">
+                <li class="nav-item">
+                        <a class="nav-link text-color" href="#">Home</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link text-white" href="#">Our Music </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#" >Inspirations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#" >Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#" >Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#" >Buy Now</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <h2 class="text-center text-light nav-h2">Immerse yourself into the world of pure & Natural sounds inspired by legends with God given tarents.</h2>
+        <button class="btn btn-outline-light w-25 rounded-0 mb-5 nav-btn">Join US</button>
+    </header>
+    <section class="container bg-color mt-lg-n5">
+        <p class="text-center section-p">ORYX ACCAPELLA GROUP</p>
+        <hr class="w-25 bg-success">
+        <div class="row py-4">
+            <div class="col-4 px-lg-5">
+                some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing
+            </div>
+            <div class="col-4 px-lg-5">
+            some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing
+            </div>
+            <div class="col-4 px-lg-5">
+            some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 px-lg-0">
+                <img src="img/front1.jpeg"  class="m-0" width="100%" alt="">
+            </div>
+            <div class="col-6 px-lg-0">
+            <img src="img/front2.jpeg" width="100%" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="container">
+        <div class="row py-4">
+            <div class="col-6 px-lg-1">
+            some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing
+            </div>
+            <div class="col-6 px-lg-1">
+            some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing some text i just thought of typing some text i just thought of typingsome text i just thought of typing some text i just thought of typing
+            </div>
+        </div>
+        <button class="btn btn-outline-dark w-50 btx rounded-0 mt-5 mb-5">Contact US</button>
+    </section>
+    <footer class="footer text-center">
+        <img src="img/sign.png" width="12%" alt="">
+        <div class="social m-4">
+            <a href="#" class="m-3"><i class="fab fa-facebook fa-2x text-color"></i></a>
+            <a href="#" class="m-3"><i class="fab fa-twitter fa-2x text-color"></i></a>
+            <a href="#" class="m-3"><i class="fab fa-instagram fa-2x text-color"></i></a>
+            <a href="#" class="m-3"><i class="fab fa-youtube fa-2x text-color"></i></a>
+            <a href="#" class="m-3"><i class="fab fa-vemeo fa-2x text-color"></i></a>
+        </div>
+        <div class="align-content-center">
+            &copy 2020 oryx accapella group | Deign & Developed by Studio4nx.com
+        </div>
+    </footer>
+    <!-- jQuery -->
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
